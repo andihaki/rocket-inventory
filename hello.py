44a2e81b8fba354beaa5d6f3533de46da0f3fe61
Andi Hakim Arif <andi.hakim.arif@gmail.com>
2020-03-17 21:49:31 +0700

    change tema to car inventory. styling with milligrams

--- hello.py
+++ hello.py
@@ -1,17 +1,19 @@
 from flask import Flask, url_for, request, render_template, jsonify, redirect
 from markupsafe import escape
+import json
+import os
 app = Flask(__name__)
 
 file_path = "data.json"
-file_2_path = "data_bagian.json"
+file_2_path = "data_2.json"
 
 def find_relation(word, array):
     founded = False
     founded_word = ""
 
     for index, row in enumerate(array):
-        if row['kode_bagian'] == word:
-            founded_word = row['nama_bagian']
+        if row['id_relation'] == word:
+            founded_word = row['tipe']
             founded = True
     
     if founded:
@@ -21,78 +23,57 @@ def find_relation(word, array):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    import json
     import os
 
     # instansi variabel
-    data_karyawan = []
-    new_data_karyawan = []
-    data_bagian = []
-    new_data_bagian = []
+    data = []
+    new_data_2 = []
+    data_2 = []
     keyword = ""
-    columns = ['NIP', 'Golongan', 'Nama', 'Nama Bagian']
-    columns_sort = ['nip', 'golongan', 'nama', 'nama_bagian']
+    columns = ['ID', 'Nama', 'Telepon', 'Tipe Mobil']
+    columns_sort = ['id', 'nama', 'telepon', 'kode_relation']
 
     # data join dua tabel
     data_join = []
 
-    # baca data.json, simpan di variabel data_karyawan
+    # baca data.json, simpan di variabel data
     # jika file tidak ada, maka buat file tersebut
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
-            data_karyawan = json.load(file)
+            data = json.load(file)
     else:
         with open(file_path, "w") as file:
             json.dump([], file, indent=2)
 
-    # baca data.json, simpan di variabel data_karyawan
+    # baca data.json, simpan di variabel data
     # jika file tidak ada, maka buat file tersebut
     if os.path.exists(file_2_path):
         with open(file_2_path, "r") as file:
-            data_bagian = json.load(file)
+            data_2 = json.load(file)
     else:
         with open(file_2_path, "w") as file:
             json.dump([], file, indent=2)
     
     # copy without reference
     from copy import deepcopy
-    data_join = deepcopy(data_karyawan)  
+    data_join = deepcopy(data)  
 
     # looping semua data karyawan
     for index, row in enumerate(data_join):
-        data_join[index]['nama_bagian'] = find_relation(row['kode_bagian'], data_bagian)
-
-    # baca data.json, simpan di variabel data_karyawan
-    # jika file tidak ada, maka buat file tersebut
-    if os.path.exists(file_2_path):
-        with open(file_2_path, "r") as file:
-            data_bagian = json.load(file)
-    else:
-        with open(file_2_path, "w") as file:
-            json.dump([], file, indent=2)
+        data_join[index]['tipe'] = find_relation(row['kode_relation'], data_2)
 
     # jika data dikiriim dengan method POST
     # maka update data karyawan di data.json
     if request.method == 'POST':
-        nip = request.form.get('nip', '')
-        golongan = request.form.get('golongan', '')
-        nama = request.form.get('nama', '')
-        kode_bagian = request.form.get('kode_bagian', '')
         keyword = request.form.get('keyword', '')
         sort_by = request.form.getlist('sort_by')
 
-        # untuk tambah data bagian
-        nama_bagian = request.form.get('nama_bagian', '')
-
         # jika ada keyword,
         if keyword:
-            for data in data_karyawan:
-                if data['nip'] == keyword:
-                    data_karyawan = [data]
-
-                    break
-                else:
-                    data_karyawan = []
+            for d in data_join:
+                is_in_list = d['id'] == keyword or d['nama'].lower().find(keyword.lower()) != -1 or d['telepon'].find(keyword) != -1 or d['tipe'].lower().find(keyword.lower()) != -1
+                if not is_in_list:
+                    data_join.remove(d)
         elif sort_by:
             data_length = len(data_join) - 1
             # sorting data
@@ -106,94 +87,88 @@ def index():
                             data_join[index+1] = temp
                     index += 1
                 data_length -= 1
+    
+    # tampilkan data dengan template index.html
+    return render_template('index.html', data=data_join, keyword=keyword, columns=columns, columns_sort=columns_sort)
 
-        elif nip:
+
+@app.route('/tambah-data', methods=['GET', 'POST'])
+def addData():
+    new_data = {}
+    data = []
+    new_data_2 = []
+
+    # baca data.json, simpan di variabel data
+    with open(file_path, "r") as file:
+        data = json.load(file)
+
+    if request.method == 'POST':
+        id = request.form.get('id', '')
+        telepon = request.form.get('telepon', '')
+        nama = request.form.get('nama', '')
+        kode_relation = request.form.get('kode_relation', '')
+
+        if id:
             new_data = {
-                "nip": nip,
-                "golongan": golongan,
+                "id": id,
+                "telepon": telepon,
                 "nama": nama,
-                "kode_bagian": kode_bagian
+                "kode_relation": kode_relation
             }
 
-            is_data_exist = any(d['nip'] == nip for d in data_karyawan)
+            is_data_exist = any(d['id'] == id for d in data)
             # simpan ke variabel data karyawan baru
             if new_data and not is_data_exist:
-                # data_karyawan.append(new_data)
-                new_data_karyawan = data_karyawan + [new_data]
+                # data.append(new_data)
+                new_data_2 = data + [new_data]
 
             # simpan ke variabel, data karyawan yang di edit
             else:
                 # cari data sesuai 'id'
-                for index, row in enumerate(data_karyawan):
-                    if row['nip'] == nip:
-                        # ganti data di index sesuai nip dengan data yang baru
-                        data_karyawan[index] = new_data
-            # data_karyawan = new_data_karyawan
+                for index, row in enumerate(data):
+                    if row['id'] == id:
+                        # ganti data di index sesuai id dengan data yang baru
+                        data[index] = new_data
             
             # tulis ke file
             with open(file_path, "w") as file:
-                # jika ada data baru, maka tulis new_data_karyawan
+                # jika ada data baru, maka tulis new_data_2
                 json.dump(
-                    new_data_karyawan if new_data_karyawan else data_karyawan, file, indent=2)
-        
-        elif kode_bagian:
-            new_data = {
-                "kode_bagian": kode_bagian,
-                "nama_bagian": nama_bagian
-            }
-            is_data_exist = any(d['kode_bagian'] == kode_bagian for d in data_bagian)
-            if new_data and not is_data_exist:
-                new_data_bagian = data_bagian + [new_data]
-            data_bagian = new_data_bagian
+                    new_data_2 if new_data_2 else data, file, indent=2)
 
-            # tulis ke file
-            with open(file_2_path, "w") as file:
-                json.dump(new_data_bagian if new_data_bagian else data_bagian, file, indent=2)
-        
-        # refresh page
-        redirect(url_for('index'))
-
-    
-    # tampilkan data_karyawan dengan template index.html
-    return render_template('index.html', data=data_join, keyword=keyword, columns=columns, columns_sort=columns_sort)
-
-
-@app.route('/tambah-data-karyawan')
-def addData():
-    return render_template('add-data-form.html')
+    add_done = True if new_data else False
+    return render_template('add-data-form.html', data=new_data, add_done=add_done)
 
 
 @app.route('/edit/<id>')
 def editData(id):
-    data_karyawan = []
+    data = []
 
-    # baca data.json, simpan di variabel data_karyawan
+    # baca data.json, simpan di variabel data
     with open(file_path, "r") as file:
-        import json
-        data_karyawan = json.load(file)
+        data = json.load(file)
 
     # cari data sesuai 'id'
-    for row in data_karyawan:
-        if row['nip'] == id:
-            data_karyawan = row
+    for row in data:
+        if row['id'] == id:
+            data = row
 
-    return render_template('add-data-form.html', data=data_karyawan)
+    return render_template('add-data-form.html', data=data)
 
 
 @app.route('/hapus/<id>')
 def removeData(id):
-    data_karyawan = []
+    data = []
     deleted_data = {}
     deleted_index = 0
 
-    # baca data.json, simpan di variabel data_karyawan
+    # baca data.json, simpan di variabel data
     with open(file_path, "r") as file:
-        import json
-        data_karyawan = json.load(file)
+        data = json.load(file)
 
     # cari data sesuai id
-    for index, row in enumerate(data_karyawan):
-        if row['nip'] == id:
+    for index, row in enumerate(data):
+        if row['id'] == id:
             # simpan data karyawan dan index yang akan dihapus
             deleted_data = row
             deleted_index = index
@@ -204,15 +179,53 @@ def removeData(id):
         # ambil data kecuali yang sudah di hapus
         # mulai dari 0 sampai index data yang sudah di hapus
         # digabung dengan index + 1 data yang sudah di hapus sampai panjang data
-        new_data_karyawan = data_karyawan[0:deleted_index] + \
-            data_karyawan[deleted_index+1:len(data_karyawan)]
+        new_data_2 = data[0:deleted_index] + \
+            data[deleted_index+1:len(data)]
 
         # tulis data setelah di hapus ke file
         with open(file_path, "w") as file:
-            json.dump(new_data_karyawan, file, indent=2)
+            json.dump(new_data_2, file, indent=2)
 
     return render_template('remove-data.html', data=deleted_data)
 
-@app.route('/tambah-data-bagian')
+@app.route('/tambah-data-relasi', methods=['GET', 'POST'])
 def addData2():
-    return render_template('add-data-form-2.html')
+    data_2 = []
+    new_data = {}
+
+    # baca data.json, simpan di variabel data
+    # jika file tidak ada, maka buat file tersebut
+    if os.path.exists(file_2_path):
+        with open(file_2_path, "r") as file:
+            data_2 = json.load(file)
+    else:
+        with open(file_2_path, "w") as file:
+            json.dump([], file, indent=2)
+    
+    if request.method == 'POST':
+        # untuk tambah data bagian
+        id_relation = request.form.get('id_relation', '')
+        tipe = request.form.get('tipe', '')
+
+        if id_relation:
+            new_data = {
+                "id_relation": id_relation,
+                "tipe": tipe
+            }            
+            is_data_exist = any(d['id_relation'] == id_relation for d in data_2)
+            if new_data and not is_data_exist:
+                data_2 = data_2 + [new_data]
+             # simpan ke variabel, data karyawan yang di edit
+            else:
+                # cari data sesuai 'id'
+                for index, row in enumerate(data_2):
+                    if row['id_relation'] == id_relation:
+                        # ganti data di index sesuai id dengan data yang baru
+                        data_2[index] = new_data
+
+            # tulis ke file
+            with open(file_2_path, "w") as file:
+                json.dump(data_2, file, indent=2)
+
+    add_done = True if new_data else False
+    return render_template('add-data-form-2.html', add_done=add_done)
